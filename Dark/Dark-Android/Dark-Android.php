c4fe9c348bf3a82a7e7ae32a1a5b097353125888
MertKx0 <121361736+MertKx0@users.noreply.github.com>
2023-10-21 21:52:25 +0300

    fchg

--- Dark/Dark-Android/Dark-Android.php
+++ Dark/Dark-Android/Dark-Android.php
@@ -175,6 +175,25 @@
             <div class="orders" style="margin-left: 5px;">
 
                 <div class="resim" style="float: left;">
+                    <img src="../images/android/TruckSimulator.webp">
+                </div>
+                <div class="yazi" style="float: left;">
+                    <b>Truck</b><br>
+                    <b>Simulator</b>
+                </div><br>
+                <div class="link" style="float: left; margin-top: 1rem; font-size: 1.2rem;">
+                    <button>
+                        <a href="https://d.apkpure.com/b/XAPK/com.zuuks.truck.simulator.ultimate?version=latest"><i class='bx bxs-download'></i><b>İ N D İ R</b></a>
+                    </button>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="bottom-data">
+            <div class="orders" style="margin-left: 5px;">
+
+                <div class="resim" style="float: left;">
                     <img src="../images/android/Roblox.webp">
                 </div>
                 <div class="yazi" style="float: left;">
--- Dark/Windows/Dark-Windows.php
+++ Dark/Windows/Dark-Windows.php
@@ -62,46 +62,119 @@
     <main>
         <div class="header">
             <div class="left">
-                <h1>Hoşgeldiniz</h1>
+                <h1>Windows</h1>
             </div>
         </div>
 
-        <div class="bottom-data">
+        <div class="bottom-data" style="float: left; margin-left: 5px;">
             <div class="orders">
 
-                Merhaba
-
-            </div>
-        </div>
-
-        <div class="bottom-data">
-            <div class="orders">
-
-                Merhaba
-
-            </div>
-        </div>
-
-        <div class="bottom-data">
-            <div class="orders">
-
-                Merhaba
+                <div class="resim" style="float: left;">
+                    <img src="../images/android/Roblox.webp">
+                </div>
+                <div class="yazi" style="float: left;">
+                    <b>ROBLOX</b>
+                </div><br>
+                <div class="link" style="float: left; margin-top: 1rem; font-size: 1.2rem;">
+                    <button>
+                        <a href="https://d.apkpure.com/b/APK/com.roblox.client?version=latest"><i class='bx bxs-download'></i><b>İ N D İ R</b></a>
+                    </button>
+                </div>
 
             </div>
         </div>
 
-        <div class="bottom-data">
-            <div class="orders">
+        <div class="bottom-data" style="float: left;">
+            <div class="orders" style="margin-left: 5px;">
 
-                Merhaba
+                <div class="resim" style="float: left;">
+                    <img src="../images/android/BrawlStars.webp">
+                </div>
+                <div class="yazi" style="float: left;">
+                    <b>Brawl</b><br>
+                    <b>Stars</b>
+                </div><br>
+                <div class="link" style="float: left; margin-top: 1rem; font-size: 1.2rem;">
+                    <button>
+                        <a href="https://d.apkpure.com/b/APK/com.supercell.brawlstars?version=latest"><i class='bx bxs-download'></i><b>İ N D İ R</b></a>
+                    </button>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="bottom-data" style="float: left;">
+            <div class="orders" style="margin-left: 5px;">
+
+                <div class="resim" style="float: left;">
+                    <img src="../images/android/ApexRacing.webp">
+                </div>
+                <div class="yazi" style="float: left;">
+                    <b>Apex</b><br>
+                    <b>Racing</b>
+                </div><br>
+                <div class="link" style="float: left; margin-top: 1rem; font-size: 1.2rem;">
+                    <button>
+                        <a href="https://d.apkpure.com/b/XAPK/com.PURE.Puredrifting?version=latest"><i class='bx bxs-download'></i><b>İ N D İ R</b></a>
+                    </button>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="bottom-data" style="float: left;">
+            <div class="orders" style="margin-left: 5px;">
+
+                <div class="resim" style="float: left;">
+                    <img src="../images/android/AmongUs.webp">
+                </div>
+                <div class="yazi" style="float: left;">
+                    <b>Among</b><br>
+                    <b>Us</b>
+                </div><br>
+                <div class="link" style="float: left; margin-top: 1rem; font-size: 1.2rem;">
+                    <button>
+                        <a href="https://d.apkpure.com/b/APK/com.innersloth.spacemafia?version=latest"><i class='bx bxs-download'></i><b>İ N D İ R</b></a>
+                    </button>
+                </div>
 
             </div>
         </div>
 
         <div class="bottom-data">
-            <div class="orders">
+            <div class="orders" style="margin-left: 5px;">
 
-                Merhaba
+                <div class="resim" style="float: left;">
+                    <img src="../images/android/SahinDrift.webp">
+                </div>
+                <div class="yazi" style="float: left;">
+                    <b>Şahin</b><br>
+                    <b>Drift</b>
+                </div><br>
+                <div class="link" style="float: left; margin-top: 1rem; font-size: 1.2rem;">
+                    <button>
+                        <a href="https://d.apkpure.com/b/APK/com.clapgames.sahinarabadriftoyunu?version=latest"><i class='bx bxs-download'></i><b>İ N D İ R</b></a>
+                    </button>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="bottom-data">
+            <div class="orders" style="margin-left: 5px;">
+
+                <div class="resim" style="float: left;">
+                    <img src="../images/android/TruckSimulator.webp">
+                </div>
+                <div class="yazi" style="float: left;">
+                    <b>Truck</b><br>
+                    <b>Simulator</b>
+                </div><br>
+                <div class="link" style="float: left; margin-top: 1rem; font-size: 1.2rem;">
+                    <button>
+                        <a href="https://d.apkpure.com/b/XAPK/com.zuuks.truck.simulator.ultimate?version=latest"><i class='bx bxs-download'></i><b>İ N D İ R</b></a>
+                    </button>
+                </div>
 
             </div>
         </div>
